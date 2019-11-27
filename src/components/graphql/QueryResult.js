43c42bb101cd8229f22f983261ef25675f8c4ad2
AleksZ13ru <a.zaykin@list.ru>
2019-11-28 01:19:55 +0300

    Edit Canvas

--- src/components/graphql/QueryResult.js
+++ src/components/graphql/QueryResult.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { useQuery } from '@apollo/react-hooks';
-import { gql } from 'apollo-boost';
+import {useQuery} from '@apollo/react-hooks';
+import {gql} from 'apollo-boost';
+import {getDirectiveValues} from "graphql";
+import Tooltip from "@material-ui/core/Tooltip";
 
 
 const QUERY_RESULT = gql`
@@ -13,16 +15,26 @@ const QUERY_RESULT = gql`
 `;
 
 export default function QueryResult() {
-  const { loading, error, data } = useQuery(QUERY_RESULT);
+    const {loading, error, data} = useQuery(QUERY_RESULT);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :( {error.message}</p>;
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :( {error.message}</p>;
+    const as = JSON.parse(data.result.value);
+    console.log(typeof data.result.value);
+    console.log(typeof as);
+    return as.map((v, i) => (
 
-  return (
-    <div>
-      <p>
-        {data.result.value}
-      </p>
-    </div>
-  );
+        <Tooltip title='08:00 - {v}' placement="top">
+            <rect data-toggle="tooltip" height="20" width="1" x={i} y="0" fill="#81C784"/>
+        </Tooltip>
+    ));
+
+
+// (
+//   <div>
+//     <p>
+//       {data.result.value}
+//     </p>
+//   </div>
+// );
 }
